aeee8ae0bdde65a62a6b14a801402004dbe7ee10
Dane Naebers <danenaebers@gmail.com>
2019-03-28 12:02:16 +0100

    implement repository

--- src/clients/concrete/mongoDB.ts
+++ src/clients/concrete/mongoDB.ts
@@ -1,5 +1,6 @@
 import { injectable } from "inversify";
 import * as Mongo from "mongodb";
+import Logger from "../../logger/logger";
 import { IClient } from "../interfaces/client";
 
 @injectable()
@@ -7,11 +8,15 @@ export class MongoDB implements IClient<Mongo.Db> {
     private client: Mongo.MongoClient;
 
     public async connectAsync(): Promise<Mongo.Db> {
+        Logger.info("Opening MongoDB connection");
+
         this.client = await Mongo.connect("mongodb://localhost:27017");
         return await this.client.db("db_name");
     }
 
     public async disconnectAsync(): Promise<void> {
+        Logger.info("Closing MongoDB connection");
+        
         await this.client.close();
     }
 }
--- src/repositories/concrete/mongoBlockchanRepository.ts
+++ src/repositories/concrete/mongoBlockchanRepository.ts
@@ -2,6 +2,7 @@ import { Block } from "@blockr/blockr-models";
 import { inject, injectable } from "inversify";
 import * as Mongo from "mongodb";
 import { IClient, MongoDB } from "../../clients";
+import Logger from "../../logger/logger";
 import { IBlockchainRepository } from "../interfaces/blockchainRepository";
 
 /**
@@ -17,10 +18,15 @@ export class MongoBlockchainRepository implements IBlockchainRepository {
 
     public async getBlockchainAsync(): Promise<Block[]> {
         try {
+            Logger.info("Get blockchain");
+
             const database = await this.client.connectAsync();
             const collection = database.collection("blocks");
+
             return await collection.find().toArray();
         } catch (error) {
+            Logger.error(error);
+
             throw error;
         } finally {
             await this.client.disconnectAsync();
@@ -28,19 +34,59 @@ export class MongoBlockchainRepository implements IBlockchainRepository {
     }
 
     public async getBlockAsync(blockNumber: number): Promise<Block> {
-        throw new Error("Method not implemented.");
+        try {
+            Logger.info("Get block by number");
+
+            const database = await this.client.connectAsync();
+            const collection = database.collection("blocks");
+
+            return await collection.findOne({ "blockHeader.blockNumber": blockNumber });
+        } catch (error) {
+            throw error;
+        } finally {
+            this.client.disconnectAsync();
+        }
     }
 
     public async setBlocksAsync(blocks: Block[]): Promise<void> {
-        throw new Error("Method not implemented.");
+        try {
+            Logger.info("Set multiple blocks");
+
+            const database = await this.client.connectAsync();
+            const collection = database.collection("blocks");
+
+            await collection.insertMany(blocks);
+        } catch (error) {
+            throw error;
+        } finally {
+            this.client.disconnectAsync();
+        }
     }
 
     public async setBlockAsync(block: Block): Promise<void> {
-        throw new Error("Method not implemented.");
+        try {
+            Logger.info("Set single block");
+
+            const database = await this.client.connectAsync();
+            const collection = database.collection("blocks");
+
+            await collection.insertOne(block);
+        } catch (error) {
+            throw error;
+        } finally {
+            this.client.disconnectAsync();
+        }
     }
 
     public async deleteBlocksASync(blockNumbers: number[]): Promise<void> {
-        throw new Error("Method not implemented.");
+        try {
+            Logger.info("Delete multiple blocks");
+
+            const database = await this.client.connectAsync();
+            const collection = database.collection("blocks");
+
+            await collection.deleteOne({"blockHeader.blockNumber": blockNumbers})
+        }
     }
 
     public async deleteBlockAsync(blockNumber: number): Promise<void> {
