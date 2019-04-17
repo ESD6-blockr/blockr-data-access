d7dfcebb1e1dac7f341a1ef428548ddd67a9d099
Dane Naebers <danenaebers@gmail.com>
2019-04-17 11:57:10 +0200

    remove configuration responsibility

--- src/injection/container.ts
+++ src/injection/container.ts
@@ -1,8 +1,6 @@
 import { Container } from "inversify";
 import * as Mongo from "mongodb";
 import { IClient, LevelDB, MongoDB } from "../clients";
-import { IClientConfiguraton, MongoDBConfiguration } from "../configurations";
-import { loadApplicationSettings } from "../utils/configLoader";
 
 /**
  * Composition root
@@ -15,11 +13,6 @@ const DIContainer = new Container();
 /**
  * Appsettings file that holds application configurations
  */
-const appsettings = loadApplicationSettings();
-
-// Bind configurations
-DIContainer.bind<IClientConfiguraton>(MongoDBConfiguration)
-.toConstantValue(new MongoDBConfiguration(appsettings.mongodb.connectionString, appsettings.mongodb.database));
 
 // Bind clients
 DIContainer.bind<IClient<Mongo.Db>>(MongoDB).toSelf().inTransientScope();
