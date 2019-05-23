9020ffebc64f73511dad0504cc927f3b02d16665
Sven <s_devries@live.nl>
2019-05-23 11:18:07 +0200

    BLO-22 fix error 4.0

--- src/repositories/concrete/mongoStateRepository.ts
+++ src/repositories/concrete/mongoStateRepository.ts
@@ -84,7 +84,7 @@ export class MongoStateRepository implements IStateRepository {
             const database = await this.client.connectAsync();
             const collection = database.collection(this.tableName);
 
-            await collection.updateOne({$set: {publicKey}}, state);
+            await collection.updateOne({publicKey}, { $set: {state}}, { upsert: true });
         } finally {
             await this.client.disconnectAsync();
         }
